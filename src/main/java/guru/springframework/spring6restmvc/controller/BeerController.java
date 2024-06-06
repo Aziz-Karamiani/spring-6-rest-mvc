566628c0687af2549c87595314b1e0cc3b4134e6
a.karamiani <aziz.karamiani@gmail.com>
2024-06-06 19:54:00 +0330

    Get beer lists

--- src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
@@ -4,8 +4,10 @@ import guru.springframework.spring6restmvc.model.Beer;
 import guru.springframework.spring6restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -13,11 +15,16 @@ import java.util.UUID;
  */
 @Slf4j
 @AllArgsConstructor
-@Controller
+@RestController
 public class BeerController {
     private final BeerService beerService;
 
-    public Beer getBeerById(UUID id){
+    @RequestMapping("/api/v1/beer")
+    public List<Beer> listBeers() {
+        return beerService.listBeers();
+    }
+
+    public Beer getBeerById(UUID id) {
 
         log.debug("Get Beer by Id - in controller");
 
